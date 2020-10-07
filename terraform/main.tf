06e820d7bd2f454f5279b6634d5c220bf1fd7ef2
dkr290 <39731809+dkr290@users.noreply.github.com>
2020-10-07 14:00:00 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,7 +32,7 @@ resource "azurerm_virtual_network" "myterraformnetwork" {
 resource "azurerm_subnet" "myterraformsubnet" {
     name                 = "subnet1"
     resource_group_name  = azurerm_resource_group.myterraformgroup.name
-    virtual_network_name = azurerm_resource_group.myterraformgroup.name
+    virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
     address_prefixes       = ["10.0.1.0/24"]
 }
 
